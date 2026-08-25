cae76633827d43403e43d8922fe612465e4cb0d3
j-nadeem <118339211+j-nadeem@users.noreply.github.com>
2026-08-25 14:47:31 +0530

    Add function to convert numbers to their spelling

---
+++ flowControl-switch.js
@@ -0,0 +1,21 @@
+function toSpelling(num){
+  switch(num){
+    case 1:
+      console.log('one');
+      break;
+    case 2:
+      console.log('two');
+      break;
+    case 3:
+      console.log('three');
+      break;
+    case 4:
+      console.log('four');
+      break;
+    case 5:
+      console.log('five');
+      break;
+    default:
+      alert('Error')
+  }
+}
